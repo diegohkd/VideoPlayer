522a9bc684cc62b3564b18a36b0cf80287c007d3
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2019-05-23 13:19:16 -0300

    Initial commit

---
+++ app/src/main/java/mobdao/com/videoplayer/VideoPlayerActivity.kt
@@ -0,0 +1,102 @@
+package mobdao.com.videoplayer
+
+import android.annotation.SuppressLint
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.DefaultRenderersFactory
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.dash.DashMediaSource
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import kotlinx.android.synthetic.main.activity_video_player.*
+
+class VideoPlayerActivity : AppCompatActivity() {
+
+    private val bandwidthMeter = DefaultBandwidthMeter()
+
+    private var player: SimpleExoPlayer? = null
+
+    private var playbackPosition: Long = 0
+    private var currentWindow: Int = 0
+    private var playWhenReady = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_player)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) initializePlayer()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (Util.SDK_INT <= 23 || player == null) initializePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT <= 23) releasePlayer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT > 23) releasePlayer()
+    }
+
+    private fun initializePlayer() {
+        if (player == null) {
+            player = ExoPlayerFactory.newSimpleInstance(
+                this,
+                DefaultRenderersFactory(this),
+                DefaultTrackSelector(), DefaultLoadControl()
+            )
+            player?.run {
+                playerView?.player = this
+                playWhenReady = this@VideoPlayerActivity.playWhenReady
+                seekTo(currentWindow, playbackPosition)
+            }
+        }
+        val mediaSource =
+            buildMediaSource(Uri.parse("http://www.youtube.com/api/manifest/dash/id/bf5bb2419360daf1/source/youtube?as=fmp4_audio_clear,fmp4_sd_hd_clear&sparams=ip,ipbits,expire,source,id,as&ip=0.0.0.0&ipbits=0&expire=19000000000&signature=51AF5F39AB0CEC3E5497CD9C900EBFEAECCCB5C7.8506521BFC350652163895D4C26DEE124209AA9E&key=ik0"))
+        player?.prepare(mediaSource, true, false)
+    }
+
+    private fun releasePlayer() {
+        player?.run {
+            playbackPosition = currentPosition
+            currentWindow = currentWindowIndex
+            this@VideoPlayerActivity.playWhenReady = playWhenReady
+            release()
+        }
+        player = null
+    }
+
+    private fun buildMediaSource(uri: Uri): MediaSource {
+        val manifestDataSourceFactory = DefaultHttpDataSourceFactory("ua")
+        val dashChunkSourceFactory = DefaultDashChunkSource.Factory(
+            DefaultHttpDataSourceFactory("ua", bandwidthMeter)
+        )
+        return DashMediaSource.Factory(dashChunkSourceFactory, manifestDataSourceFactory).createMediaSource(uri)
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        playerView?.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+    }
+}
---
+++ app/src/main/java/mobdao/com/videoplayer/VideoPlayerViewModel.kt
@@ -0,0 +1,9 @@
+package mobdao.com.videoplayer
+
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.ViewModel
+
+
+class ChatViewModel: ViewModel(), LifecycleObserver {
+
+}
